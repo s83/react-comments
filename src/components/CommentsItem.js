7f0e6a2a8d5602f32a562fdd8d7b6be8ba9f0a5c
Samuel Mansour <mansoursamuel@gmail.com>
2015-09-24 23:12:53 +0200

    enchance user component

--- src/components/CommentsItem.js
+++ src/components/CommentsItem.js
@@ -1,32 +1,100 @@
 import React from 'react';
+import { Glyphicon, ListGroupItem } from 'react-bootstrap';
+import Gravatar from 'react-gravatar';
+import moment from 'moment';
+import Spaces from 'components/Spaces';
 
 export class CommentsItem extends React.Component {
   static propTypes = {
     dispatch : React.PropTypes.func,
-    username: React.PropTypes.string.required,
-    email: React.PropTypes.string.required,
-    link: React.PropTypes.string.required,
-    content: React.PropTypes.string.required,
-    date: React.PropTypes.number.required
+    username: React.PropTypes.string,
+    email: React.PropTypes.string,
+    link: React.PropTypes.string,
+    content: React.PropTypes.string,
+    date: React.PropTypes.number
   }
 
   constructor () {
     super();
   }
 
-  getInitialState () {
-    return {};
+  renderLink () {
+    let link = '';
+    if (this.props.link) {
+      link = (
+        <span>
+          <Spaces/>
+          <a href={this.props.link}>
+            {this.props.link}
+          </a>
+        </span>
+      );
+    }
+
+    return link;
+  }
+
+  renderFooter () {
+    const date = new Date(this.props.date);
+    return (
+      <div>
+        <div className="pull-right">
+          at {date.toLocaleTimeString()}, {date.toLocaleDateString()}
+        </div>
+        <div>
+          From
+          <Spaces/>
+          <a href={`mailto:${this.props.email}`}>
+            <strong>{this.props.username}</strong>
+          </a>
+          {this.renderLink()}
+          </div>
+      </div>
+    );
+  }
+
+  renderUserUrlIcon () {
+    return (
+      <a href={this.props.link}>
+        <Glyphicon glyph="glyphicon glyphicon-link"/>
+      </a>
+    );
+  }
+
+  renderUserIcons () {
+    return (
+      <span style={{fontSize: 'xx-small'}}>
+        <Spaces/>
+        <a href={`mailto:${this.props.email}`}>
+          <Glyphicon glyph="glyphicon glyphicon-envelope"/>
+        </a>
+        <Spaces/>
+        {this.props.link ? this.renderUserUrlIcon() : ''}
+      </span>
+    );
   }
 
   render () {
     return (
-      <div className='container text-center'>
-        <div>{this.props.content}</div>
-        <div>{this.props.username}</div>
-        <div>{this.props.email}</div>
-        <div>{this.props.link}</div>
-        <div>{this.props.date}</div>
-      </div>
+      <ListGroupItem>
+          <div className="media">
+            <div className="media-left">
+              <Gravatar default="identicon" className="comment-gravatar" email={this.props.email} />
+            </div>
+            <div className="media-body">
+              <div className="pull-right comment-date">
+                <Glyphicon className="comment-middle-align" glyph="glyphicon glyphicon-time"/>
+                <Spaces/>
+                {moment(this.props.date).fromNow()}
+              </div>
+              <h2 className="comment-title">
+                <span className="comment-username">{this.props.username}</span>
+                {this.renderUserIcons()}
+              </h2>
+              {this.props.content}
+            </div>
+          </div>
+      </ListGroupItem>
     );
   }
 }
