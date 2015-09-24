692c5432497d3478257470cd76ae7fb91010c0dd
Samuel Mansour <mansoursamuel@gmail.com>
2015-09-24 11:02:58 +0200

    add constants

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+import { ADD_COMMENT } from 'constants/ActionTypes';
+
+export function addComment(comment) {
+  return { type: ADD_COMMENT, comment };
+}
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const FORM_COMMENT_ID = 'comment';
