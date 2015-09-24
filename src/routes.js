aa7f8a256550a4b33ad1be3a416734bc90704ff2
Samuel Mansour <mansoursamuel@gmail.com>
2015-09-24 16:08:31 +0200

    put better routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
 import Route from 'react-router';
 import React from 'react';
 import App  from 'containers/App';
-import Welcome from 'components/Welcome';
 import NotFound from 'components/NotFound';
 import CommentsList from 'components/CommentsList';
 import NewComment from 'components/NewComment';
 
 export default (
-  <Route component={App} path="/">
-    <Route path="home" component={Welcome}/>
-    <Route path='comment' component={CommentsList} />
+  <Route component={App}>
+    <Route path="/" component={CommentsList} />
     <Route path='/comment/new' component={NewComment} />
     <Route path="*" component={NotFound}/>
   </Route>
