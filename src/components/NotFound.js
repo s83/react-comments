2b886d48c38059a2f42611c87d59454df2cc41cf
Samuel Mansour <mansoursamuel@gmail.com>
2015-09-26 17:29:35 +0200

    enchance not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Button, Glyphicon } from 'react-bootstrap';
+import { PATH_COMMENT_LIST } from 'constants';
+import Spaces from 'components/Spaces';
 
 export class Welcome extends React.Component {
   static propTypes = {
@@ -14,6 +17,11 @@ export class Welcome extends React.Component {
       <div className='container text-center'>
         <h1>Not found</h1>
         <p>Sorry, that page does not exist</p>
+        <div style={{marginTop: 30}}>
+        <Button bsStyle="primary" href={PATH_COMMENT_LIST}>
+          Go to Homepage<Spaces size={2}/><Glyphicon glyph="glyphicon glyphicon-thumbs-up"/>
+        </Button>
+        </div>
       </div>
     );
   }
