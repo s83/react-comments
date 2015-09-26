61406d12a56aa73a0ea8fd4401784470ea52bc13
Samuel Mansour <mansoursamuel@gmail.com>
2015-09-26 17:29:48 +0200

    fix constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
+// common
+export const ROUTE_PREFIX = '/#/';
 export const FORM_COMMENT_ID = 'comment';
-export const PATH_COMMENT_NEW = '/write';
-export const PATH_COMMENT_LIST = '/';
+
+// routes
+export const ROUTE_COMMENT_NEW = 'write';
+
+// paths
+export const PATH_COMMENT_LIST = ROUTE_PREFIX;
+export const PATH_COMMENT_NEW = ROUTE_PREFIX + ROUTE_COMMENT_NEW;
