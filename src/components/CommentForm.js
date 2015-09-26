33f47eb11d546fae68e98ef27eb9156e02f555e3
Samuel Mansour <mansoursamuel@gmail.com>
2015-09-27 00:25:03 +0200

    better input feedback

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -15,15 +15,21 @@ import { PATH_COMMENT_LIST } from 'constants';
 export class CommentsItemForm extends React.Component {
   static propTypes = {
     fields: React.PropTypes.object.isRequired,
-    handleSubmit: React.PropTypes.func.isRequired
+    handleSubmit: React.PropTypes.func.isRequired,
+    handleReset: React.PropTypes.func.isRequired
   }
 
   constructor () {
     super();
   }
 
+  /**
+   * Evaluate field status based on errors and touch event
+   * @param  {object}  field
+   * @return {Boolean|null} returns null when not ready
+   */
   hasError (field) {
-    return field.error && field.touched;
+    return field.touched ? field.error !== null : null;
   }
 
   renderError (field) {
@@ -36,49 +42,71 @@ export class CommentsItemForm extends React.Component {
     );
   }
 
+  renderBsStyle () {
+    const keys = Object.keys(this.props.fields);
+    /* eslint-disable prefer-const */
+    let output = {};
+    /* eslint-enable prefer-const */
+    keys.forEach((key) => {
+      const error = this.hasError(this.props.fields[key]);
+      output[key] = error === null ? {} : { 'bsStyle': error ? 'error' : 'success' };
+    });
+    return output;
+  }
+
   render () {
-    const { fields: {username, email, link, content}, handleSubmit } = this.props;
-    const disabledSubmit = username.error || email.error || link.error || content.error;
+    const { fields: {username, email, link, content}, handleSubmit, handleReset } = this.props;
+    const disabledSubmit = !!( username.error || email.error || link.error || content.error );
+    const bsStyle = this.renderBsStyle();
+
     return (
         <form onSubmit={handleSubmit.bind(this)}>
           <Panel className="comment-panel box-shadow">
             <Input
               type="text"
               label="Full Name *"
-              bsStyle={this.hasError(username) && 'error'}
               required
-              {...username}/>
-            { this.hasError(username) && this.renderError(username) }
+              {...bsStyle.username}
+              {...username}
+            />
+          { this.hasError(username) ? this.renderError(username) : ''}
             <Input
               type="email"
               label="E-mail *"
               required
-              bsStyle={this.hasError(email) && 'error'} {...email}/>
-            { this.hasError(email) && this.renderError(email) }
+              {...bsStyle.email}
+              {...email}
+              />
+            { this.hasError(email) ? this.renderError(email) : ''}
             <Input
               type="textarea"
               label="Message *"
-              bsStyle={this.hasError(content) && 'error'} {...content}/>
-              { this.hasError(content) && this.renderError(content) }
+              {...bsStyle.content}
+              {...content}
+            />
+          { this.hasError(content) ? this.renderError(content) : ''}
             <Input
               type="text"
               label="Site"
-              bsStyle={this.hasError(link) && 'error'} {...link}/>
-            { this.hasError(link) && this.renderError(link) }
+              {...bsStyle.link}
+              {...link}
+            />
+          { this.hasError(link) ? this.renderError(link) : ''}
           </Panel>
           <ButtonToolbar>
             <Button
               href={PATH_COMMENT_LIST}
-              className
+              onClick={handleReset.bind(this)}
               >
               Cancel</Button>
             <Button
               type="submit"
-              bsStyle={disabledSubmit ? '' : 'success'}
+              bsStyle={disabledSubmit ? 'warning  ' : 'success'}
               bsSize="large"
               bsStyle={disabledSubmit ? 'primary' : 'success'}
               className="pull-right"
               disabled={disabledSubmit}
+              title={ disabledSubmit ? 'Please fill required fields or enter correct values' : 'Submit changes' }
               onClick={handleSubmit.bind(this)}>
               Save {disabledSubmit ? '' : <Glyphicon glyph="glyphicon glyphicon-ok"/>}
             </Button>
