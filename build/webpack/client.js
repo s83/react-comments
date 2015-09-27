0085cb146cda5bfb57535ba1e6b2a6323f16df2f
Samuel Mansour <mansoursamuel@gmail.com>
2015-09-28 01:11:08 +0200

    fix node dependency

--- build/webpack/client.js
+++ build/webpack/client.js
@@ -1,5 +1,5 @@
 import webpack from 'webpack';
-import config  from '../config';
+import config  from '../../config';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
